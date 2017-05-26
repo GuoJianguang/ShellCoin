ba1e4b13604c5fca33f1f8efaf55ebbb9d50fc69
guo <873191027@qq.com>
2017-05-26 22:06:42 +0800

    发现模块完成

--- ShellCoin/Discover/DiscoverCollectionViewCell.m
+++ ShellCoin/Discover/DiscoverCollectionViewCell.m
@@ -10,7 +10,21 @@
 
 #import "DiscoverCollectionViewCell.h"
 
+@implementation DiscoverGoodsModel
 
++ (id)modelWithDic:(NSDictionary *)dic
+{
+    DiscoverGoodsModel *model = [[DiscoverGoodsModel alloc]init];
+    model.goodsId   = NullToSpace(dic[@"id"]);
+    model.coverImage   = NullToSpace(dic[@"coverImage"]);
+    model.name   = NullToSpace(dic[@"name"]);
+    model.price   = NullToNumber(dic[@"price"]);
+    model.saleCount   = NullToNumber(dic[@"saleCount"]);
+    model.htmlUrl = NullToSpace(dic[@"detailUrl"]);
+    return model;
+}
+
+@end
 
 
 @implementation DiscoverCollectionViewCell
@@ -18,9 +32,19 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
-    
+    self.goodsImageView.layer.masksToBounds = YES;
+    self.goodsImageView.contentMode = UIViewContentModeScaleAspectFill;
 }
 
+
+- (void)setDataModel:(DiscoverGoodsModel *)dataModel
+{
+    _dataModel = dataModel;
+    self.goodsName.text = _dataModel.name;
+    [self.goodsImageView sd_setImageWithURL:[NSURL URLWithString:_dataModel.coverImage] placeholderImage:LoadingErrorDefaultImageSquare];
+
+    self.goodsPrice.text = [NSString stringWithFormat:@"¥%@",_dataModel.price];
+}
 
 
 @end
